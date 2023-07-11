19338082852968dd418c7d25d9b31ca5c96c5d92
João Pedro Correia Martins <tyr.mars@protonmail.com>
2023-07-11 11:20:55 -0400

    Update All

--- Projetos/bhaskara.py
+++ Projetos/bhaskara.py
@@ -4,18 +4,18 @@ import numpy as np
 from Style import fontes
 class AppBhaskara():
     def __init__(self):
-        self.root = tkinter.Tk()
+        self.root_bhaskara = tkinter.Tk()
         self.fonte_padrao = fontes.fonte_verdana_bhaskara_label
         self.tela()
         self.frame_graph()
         self.widgets()
-        self.root.mainloop()
+        self.root_bhaskara.mainloop()
 
     def tela(self):
-        self.root.geometry("800x800")
+        self.root_bhaskara.geometry("800x800")
         pass
     def frame_graph(self):
-        self.frm = Frame(self.root, bg="red")
+        self.frm = Frame(self.root_bhaskara, bg="red")
         self.frm.place(relx=0.6, rely=0.05, relwidth=0.30, relheight=0.30)
 
     def update_canvas(self, event):
@@ -48,50 +48,60 @@ class AppBhaskara():
         self.canvas.create_oval(coord_x - marker_radius, coord_y - marker_radius,
                                 coord_x + marker_radius, coord_y + marker_radius, fill="blue")
 
+        for x in range(20):
+            self.y = (self.a) * (x ** 2) + (self.b * x) + (self.c)
+            self.canvas.bind("<Configure>", self.update_canvas)
+
+            coord_x = center_x + x * (width / 20)
+            coord_y = center_y - self.y * (height / 20)
+            self.canvas.create_oval(coord_x - marker_radius, coord_y - marker_radius,
+                                    coord_x + marker_radius, coord_y + marker_radius, fill="blue")
+
     def widgets(self):
-        self.Label_a = Label(self.root, text="a:", font=self.fonte_padrao)
+        self.Label_a = Label(self.root_bhaskara, text="a:", font=self.fonte_padrao)
         self.Label_a.place(relx=0.05, rely=0.05)
 
-        self.Label_b = Label(self.root, text="b:", font=self.fonte_padrao)
+        self.Label_b = Label(self.root_bhaskara, text="b:", font=self.fonte_padrao)
         self.Label_b.place(relx=0.05, rely=0.15)
 
-        self.Label_c = Label(self.root, text="c:", font=self.fonte_padrao)
+        self.Label_c = Label(self.root_bhaskara, text="c:", font=self.fonte_padrao)
         self.Label_c.place(relx=0.05, rely=0.25)
 
-        self.Entry_a = Entry(self.root, font=self.fonte_padrao)
+        self.Entry_a = Entry(self.root_bhaskara, font=self.fonte_padrao)
         self.Entry_a.place(relx=0.2, rely=0.05, relheight=0.1, relwidth=0.3)
 
-        self.Entry_b = Entry(self.root, font=self.fonte_padrao)
+        self.Entry_b = Entry(self.root_bhaskara, font=self.fonte_padrao)
         self.Entry_b.place(relx=0.2, rely=0.15, relheight=0.1, relwidth=0.3)
 
-        self.Entry_c = Entry(self.root, font=self.fonte_padrao)
+        self.Entry_c = Entry(self.root_bhaskara, font=self.fonte_padrao)
         self.Entry_c.place(relx=0.2, rely=0.25, relheight=0.1, relwidth=0.3)
 
-        self.btnCalcular = Button(text="Calcular", command=self.bhaskara_calc)
+        self.btnCalcular = Button(self.root_bhaskara, text="Calcular", command=self.bhaskara_calc)
         self.btnCalcular.place(relx = 0.2, rely=0.40, relheight=0.1, relwidth=0.3)
 
-        self.lbl_resultado = Label(self.root, text="f(x) = ax^2+bx+c",font=self.fonte_padrao)
+        self.lbl_resultado = Label(self.root_bhaskara, text="f(x) = ax^2+bx+c", font=self.fonte_padrao)
         self.lbl_resultado.place(relx=0.2, rely=0.55)
 
     def bhaskara_calc(self):
-        a = int(self.Entry_a.get())
-        b = int(self.Entry_b.get())
-        c = int(self.Entry_c.get())
-        delta = (b**2) - 4 * (a) * (c)
+        self.a = int(self.Entry_a.get())
+        self.b = int(self.Entry_b.get())
+        self.c = int(self.Entry_c.get())
+        delta = (self.b**2) - 4 * (self.a) * (self.c)
         if delta < 0:
             print("Delta não possui valor real pois seu delta é menor que 0")
         else:
             #f(x) = ax^2 + bx + c
             raizQ_delta = delta**(1/2)
-            self.x1 = (-(b)+raizQ_delta) / (2*a)
-            self.x2 = (-(b)-raizQ_delta) / (2*a)
+            self.x1 = (-(self.b)+raizQ_delta) / (2*(self.a))
+            self.x2 = (-(self.b)-raizQ_delta) / (2*(self.a))
 
 
-            self.lbl_resultado["text"] = f"f(x) = {a}x^2+{b}x + {c}\n\nX' = {self.x1}\nX'' = {self.x2} "
+            self.lbl_resultado["text"] = f"f(x) = {self.a}x^2+{self.b}x + {self.c}\n\nX' = {self.x1}\nX'' = {self.x2} "
             print(self.x1, self.x2)
             self.canvas = tkinter.Canvas(self.frm, bg="yellow")
             self.canvas.place(relx=0, rely=0, relwidth=1, relheight=1)
             self.canvas.bind("<Configure>", self.update_canvas)
+
 
 if __name__ == "__main__":
     AppBhaskara()
--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 import tkinter
 from tkinter import *
 from Projetos import bhaskara
-class MathFuncs():
-    def bhaskara(self):
-        a = 1
-        b = -6
-        c = 5
-
-        delta = (b**2) - 4 * (a) * (c)
-        raizDelta = delta**(1/2)
-
-        x1 = (-(b) + raizDelta)/(2*a)
-        x2 = (-(b) - raizDelta)/(2*a)
-
-        print(f"X' = {x1}")
-        print(f"X'' = {x2}")
-
-class Application(MathFuncs):
+class Application():
     def __init__(self):
         self.root = tkinter.Tk()
         self.tela()
@@ -26,7 +11,7 @@ class Application(MathFuncs):
     def tela(self):
         self.root.geometry("500x700")
     def widgets(self):
-        btn1 = Button(self.root, text="Bhaskara", command=self.bhaskara)
+        btn1 = Button(self.root, text="Bhaskara", command=bhaskara.AppBhaskara)
         btn1.place(relx=0.05, rely=0.05, relheight=0.05, relwidth=0.2)
 
 
