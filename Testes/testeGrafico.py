c893813253eee39570cf554b71c1495858cfb9fb
João Pedro Correia Martins <tyr.mars@protonmail.com>
2023-07-12 16:33:23 -0400

    Update testeGrafico.py

--- Testes/testeGrafico.py
+++ Testes/testeGrafico.py
@@ -17,10 +17,23 @@ class App():
         self.canvas = Canvas(self.root, background="black")
         self.canvas.place(relx=0, rely=0, relwidth=1, relheight=1)
     def criarEixos(self):
-        height = 800
-        width = 800
+        self.height = 800
+        self.width = 800
 
-        self.canvas.create_line(0, height / 2, width, height / 2, fill="green")
-        self.canvas.create_line(width / 2, 0, width / 2, height, fill="green")
+        self.center_x = self.width / 2
+        self.center_y = self.height /2
+        self.ponto = 5
+
+        self.canvas.create_line(0, self.height / 2, self.width, self.height / 2, fill="green")
+        self.canvas.create_line(self.width / 2, 0, self.width / 2, self.height, fill="green")
+
+        self.canvas.create_oval(self.center_x - self.ponto, self.center_y - self.ponto,
+                                self.center_x + self.ponto, self.center_y + self.ponto, fill="red")
+    def MarcarCoordenada(self, x, y):
+        coord_x = self.center_x + x * (self.width / 20)
+        coord_y = self.center_y - y * (self.height / 20)
+
+        self.canvas.create_oval(coord_x - self.ponto, coord_y - self.ponto,
+                                coord_x + self.ponto, coord_y + self.ponto, fill="red")
 if __name__ == "__main__":
     App()
